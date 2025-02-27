479f0922a38692ae605c525b2ecb51378dd3652a
Zhang <jessy20172326@163.com>
2025-02-27 18:43:48 +0800

    Add self-introduction feature

--- src/main/kotlin/zhangxijie/SelfIntroduction.kt
+++ src/main/kotlin/zhangxijie/SelfIntroduction.kt
@@ -2,7 +2,7 @@ package hk.edu.hkbu.comp.comp4107.playground.zhangxijie
 
 class SelfIntroduction {
     fun selfIntroduction() {
-        println("Hello, my name is Kenny Cheng.")
+        println("Hello, my name is Zhang Xijie.")
         println("I am a student at HKBU.")
         println("I am studying Computer Science.")
         println("I am interested in machine learning.")
